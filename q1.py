d9b124f644e61e9268599b6048636b94e32b0158
GnanaMahesh <139784716+GnanaMahesh@users.noreply.github.com>
2024-04-21 00:26:05 +0530

    Add files via upload

--- q1.py
+++ q1.py
@@ -1,53 +1,206 @@
-import pickle as pkl
 import numpy as np
+import itertools
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-def pca(X: np.array, k: int) -> np.array:
-    """
-    X is an (N,a,b) array comprising N images, each of size (a,b).
-    Return (a*b,k) np array comprising the k normalised basis vectors comprising the k-dimensional subspace for all images
-    where the first column must be the most relevant principal component and so on
-    """
-    # TODO
-    N,a,b = X.shape
-    X1 = X.reshape(N,a*b)
-    mean = np.mean(X1, axis=0)
-    # Center the dataset
-    centered_X1 = X1 - mean
-     # Compute the covariance matrix
-    covariance_matrix = np.cov(centered_X1, rowvar=False)
-   # Perform eigendecomposition
-    eigenvalues, eigenvectors = np.linalg.eigh(covariance_matrix)
-   # Sort eigenvectors based on eigenvalues in descending order
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    sorted_eigenvectors = eigenvectors[:, sorted_indices]
-    # Select the top k eigenvectors
-    return sorted_eigenvectors[:, :k]
-    #END TODO
+
+''' Do not change anything in this function '''
+def generate_random_profiles(num_voters, num_candidates):
+    '''
+        Generates a NumPy array where row i denotes the strict preference order of voter i
+        The first value in row i denotes the candidate with the highest preference
+        Result is a NumPy array of size (num_voters x num_candidates)
+    '''
+    return np.array([np.random.permutation(np.arange(1, num_candidates+1)) 
+            for _ in range(num_voters)])
+
+
+def find_winner(profiles, voting_rule):
+    '''
+        profiles is a NumPy array with each row denoting the strict preference order of a voter
+        voting_rule is one of [plurality, borda, stv, copeland]
+        In STV, if there is a tie amongst the candidates with minimum plurality score in a round, then eliminate the candidate with the lower index
+        For Copeland rule, ties among pairwise competitions lead to half a point for both candidates in their Copeland score
+
+        Return: Index of winning candidate (1-indexed) found using the given voting rule
+        If there is a tie amongst the winners, then return the winner with a lower index
+    '''
+
+    winner_index = None
     
-
-def projection(X: np.array, basis: np.array):
-    """
-    X is an (N,a,b) array comprising N images, each of size (a,b).
-    basis is an (a*b,k) array comprising of k normalised vectors
-    Return (n,k) np array comprising the k dimensional projections of the N images on the normalised basis vectors
-    """
     # TODO
-    # Project data points onto the subspace formed by the principal components
-    N,a,b = X.shape
-    X1 = X.reshape(N,a*b)
-    projected_X = np.dot(X1,basis)
-    return projected_X
+
+    if voting_rule == 'plurality':
+        toppref= np.array(profiles[:,0])
+        contestants, votes = np.unique(toppref, return_counts=True)
+        max_index=0
+        for i in range(len(votes)):
+          if votes[i] > votes[max_index]:
+              max_index=i
+          elif votes[i] == votes[max_index]:
+              if(contestants[max_index] > contestants[i]):
+                  max_index=i
+        winner_index = contestants[max_index]
+
+    elif voting_rule == 'borda':
+        nvoters,ncontestants = profiles.shape
+        bordascore=np.zeros(ncontestants)
+        for i in range(ncontestants):
+            for j in range(nvoters):
+               bordascore[profiles[j,i]-1] = bordascore[profiles[j,i]-1]+ncontestants-i-1
+        winner_index=np.argmax(bordascore)
+        winner_index = winner_index+1
+
+    elif voting_rule == 'stv':
+        nvoters,ncontestants = profiles.shape
+        eliminated = np.zeros(ncontestants, dtype=int)
+        for i in range(ncontestants-1):
+            toppref=np.zeros(nvoters, dtype=int)
+            for vo in range(nvoters):
+                for co in range(ncontestants):
+                    if(eliminated[profiles[vo,co]-1] == 0):
+                        toppref[vo] = profiles[vo,co]
+                        break
+            votes = np.zeros(ncontestants, dtype=int)
+            for l in range(nvoters):
+                votes[toppref[l]-1] = votes[toppref[l]-1]+1
+            for j in range(ncontestants):
+              if eliminated[j]==0:
+                eliminate_index=j
+                break
+            for j in range(ncontestants):
+              if votes[j] < votes[eliminate_index] and eliminated[j]==0:
+                eliminate_index=j
+            eliminated[eliminate_index]=1
+        
+        for i in range(ncontestants):
+            if eliminated[i] == 0:
+                winner_index = i+1
+
+    elif voting_rule == 'copeland':
+        nvoters,ncontestants = profiles.shape
+        copeland = np.zeros(ncontestants, dtype=int)
+        for c in range(ncontestants):
+            better = np.zeros(ncontestants, dtype=int)
+            for i in range(nvoters):
+                for j in range(ncontestants):
+                    if profiles[i,j] != c+1 :
+                        better[profiles[i,j]-1] = better[profiles[i,j]-1]+1
+                    else:
+                        break
+            for i in range(ncontestants):
+                if ((2 * better[i]) < nvoters):
+                    copeland[c]=copeland[c]+2
+                elif ((2 * better[i]) == nvoters):
+                    copeland[c]=copeland[c]+1
+        winner_index = np.argmax(copeland)           
+        winner_index = winner_index+1
+
     # END TODO
+
+    return winner_index
+
+
+def find_winner_average_rank(profiles, winner):
+    '''
+        profiles is a NumPy array with each row denoting the strict preference order of a voter
+        winner is the index of the winning candidate for some voting rule (1-indexed)
+
+        Return: The average rank of the winning candidate (rank wrt a voter can be from 1 to num_candidates)
+    '''
+
+    average_rank = None
+
+    # TODO
+    average_rank =0
+    nvoters,ncontestants = profiles.shape
+    for i in range(nvoters):
+        for j in range(ncontestants):
+            if profiles[i,j] == winner :
+                average_rank = average_rank+j+1
+    average_rank = average_rank/nvoters
+    # END TODO
+
+    return average_rank
+
+
+def check_manipulable(profiles, voting_rule, find_winner):
+    '''
+        profiles is a NumPy array with each row denoting the strict preference order of a voter
+        voting_rule is one of [plurality, borda, stv, copeland]
+        find_winner is a function that takes profiles and voting_rule as input, and gives the winner index as the output
+        It is guaranteed that there will be at most 8 candidates if checking manipulability of a voting rule
+
+        Return: Boolean representing whether the voting rule is manipulable for the given preference profiles
+    '''
+
+    manipulable = None
+
+    # TODO
+    nvoters,ncontestants = profiles.shape
+    winner = find_winner(profiles,voting_rule)
+    manipulable = False
+    for i in range(nvoters):
+        if(manipulable == True):
+            break
+        profiles1=profiles.copy()
+        perms = list(itertools.permutations(np.array(profiles[i,:])))
+        permut = np.array(perms)
+        rows,colums = permut.shape
+        for j in range(rows):
+            profiles1[i,:]=np.array(permut[j,:])
+            winner1 = find_winner(profiles1,voting_rule)
+            index1 = np.where(np.array(profiles[i,:]) == winner1)[0]
+            index = np.where(np.array(profiles[i,:]) == winner)[0]
+            if(index1 < index):
+                manipulable=True
+                break
+    # END TODO
+
+    return manipulable
 
 
 if __name__ == '__main__':
-    mnist_data = 'mnist.pkl'
-    with open(mnist_data, 'rb') as f:
-        data = pkl.load(f)
-    # Now you can work with the loaded object
-    X=data[0]
-    y=data[1]
-    k=10
-    basis = pca(X,k)
-    print(projection(X,basis))
+    np.random.seed(420)
+
+    num_tests = 200
+    voting_rules = ['plurality', 'borda', 'stv', 'copeland']
+
+    average_ranks = [[] for _ in range(len(voting_rules))]
+    manipulable = [[] for _ in range(len(voting_rules))]
+    for _ in tqdm(range(num_tests)):
+        # Check average ranks of winner
+        num_voters = np.random.choice(np.arange(80, 150))
+        num_candidates = np.random.choice(np.arange(10, 80))
+        profiles = generate_random_profiles(num_voters, num_candidates)
+
+        for idx, rule in enumerate(voting_rules):
+            winner = find_winner(profiles, rule)
+            avg_rank = find_winner_average_rank(profiles, winner)
+            average_ranks[idx].append(avg_rank / num_candidates)
+
+        # Check if profile is manipulable or not
+        num_voters = np.random.choice(np.arange(7, 11))
+        num_candidates = np.random.choice(np.arange(3, 7))
+        profiles = generate_random_profiles(num_voters, num_candidates)
+        
+        for idx, rule in enumerate(voting_rules):
+            manipulable[idx].append(check_manipulable(profiles, rule, find_winner))
+
+
+    # Plot average ranks as a histogram
+    for idx, rule in enumerate(voting_rules):
+        plt.hist(average_ranks[idx], alpha=0.8, label=rule)
+
+    plt.legend()
+    plt.xlabel('Fractional average rank of winner')
+    plt.ylabel('Frequency')
+    plt.savefig('average_ranks.jpg')
     
+    # Plot bar chart for fraction of manipulable profiles
+    manipulable = np.sum(np.array(manipulable), axis=1)
+    manipulable = np.divide(manipulable, num_tests)
+    plt.clf()
+    plt.bar(voting_rules, manipulable)
+    plt.ylabel('Manipulability fraction')
+    plt.savefig('manipulable.jpg')
--- q2.py
+++ q2.py
@@ -1,68 +1,167 @@
-import numpy as np
-import pickle as pkl
+import matplotlib.pyplot as plt
+from typing import Dict, List, Tuple
 
-class LDA:
-    def __init__(self,k):
-        self.n_components = k
-        self.linear_discriminants = None
+def Gale_Shapley(suitor_prefs, reviewer_prefs) -> Dict[str, str]:
+    '''
+        Gale-Shapley Algorithm for Stable Matching
 
-    def fit(self, X, y):
-        """
-        X: (n,d,d) array consisting of input features
-        y: (n,) array consisting of labels
-        return: Linear Discriminant np.array of size (d*d,k)
-        """
-        # # TODO
-        n, d, _ = X.shape
-        X = X.reshape(n, -1)
-        classes = np.unique(y)
-        num_classes = len(classes)
-        class_means = np.array([np.mean(X[y == c], axis=0) for c in classes])
-        overall_mean = np.mean(X, axis=0)
-        Sw = np.zeros((d*d, d*d))
-        for c in classes:
-            class_data = X[y == c]
-            class_mean = class_means[c]
-            scatter_matrix = np.dot((class_data - class_mean).T, (class_data - class_mean))
-            Sw += scatter_matrix
+        Parameters:
 
-        # Compute between-class scatter matrix Sb
-        Sb = np.zeros((d*d, d*d))
-        for c in classes:
-            n_c = X[y == c].shape[0]
-            class_mean = class_means[c].reshape(-1, 1)
-            overall_mean_reshaped = overall_mean.reshape(-1, 1) 
-            Sb += n_c * np.outer((class_mean - overall_mean_reshaped), (class_mean - overall_mean_reshaped))
-        eigenvalues, eigenvectors = np.linalg.eig(np.linalg.pinv(Sw).dot(Sb))
-        eig_pairs = []
-        for i in range(len(eigenvalues)):
-            eig_pairs.append((np.abs(eigenvalues[i]), eigenvectors[:,i]))
-        eig_pairs.sort(key=lambda x: x[0], reverse=True)
+        suitor_prefs: dict - Dictionary of suitor preferences
+        reviewer_prefs: dict - Dictionary of reviewer preferences
 
-        linear_discriminants = []
-        for i in range(0, self.n_components):
-            linear_discriminants.append(eig_pairs[i][1].reshape(d*d, 1))
-        self.linear_discriminants = np.hstack(linear_discriminants)
-        return self.linear_discriminants  
-        #END TODO 
-    
-    def transform(self, X, w):
-        """
-        w:Linear Discriminant array of size (d*d,1)
-        return: np-array of the projected features of size (n,k)
-        """
-        # TODO
-        projected = np.dot(X.reshape(len(X), -1), w)
-        return projected                   # Modify as required
-        # END TODO
+        Returns:
+
+        matching: dict - Dictionary of suitor matching with reviewer
+    '''
+
+    matching = {}
+    ## TODO: Implement the Gale-Shapley Algorithm
+    suitor = list(suitor_prefs.keys())
+    reviewer = []
+    while(suitor):
+        for key in suitor:
+            if(key not in suitor):
+                break
+            else:
+                expectmatchesposi = suitor_prefs[key]
+                for expectmatch in expectmatchesposi:
+                    if expectmatch not in reviewer:
+                        matching[key]=expectmatch
+                        suitor.pop(suitor.index(key))
+                        reviewer.append(expectmatch)
+                        break
+                    else:
+                        presentmatch = [key1 for key1 in matching.keys() if matching[key1] == expectmatch][0]
+                        if reviewer_prefs[expectmatch].index(key)<reviewer_prefs[expectmatch].index(presentmatch):
+                            matching.pop(presentmatch)
+                            suitor.append(presentmatch)
+                            matching[key]=expectmatch
+                            suitor.pop(suitor.index(key))
+                            break
+     
+    ## END TODO
+
+    return matching
+
+def avg_suitor_ranking(suitor_prefs: Dict[str, List[str]], matching: Dict[str, str]) -> float:
+    '''
+        Calculate the average ranking of suitor in the matching
+        
+        Parameters:
+        
+        suitor_prefs: dict - Dictionary of suitor preferences
+        matching: dict - Dictionary of matching
+        
+        Returns:
+        
+        avg_suitor_ranking: float - Average ranking of suitor
+    '''
+
+    avg_suitor_ranking = 0
+
+    ## TODO: Implement the average suitor ranking calculation
+    for suitor in matching.keys():
+        avg_suitor_ranking = avg_suitor_ranking+suitor_prefs[suitor].index(matching[suitor]) + 1
+
+    avg_suitor_ranking = avg_suitor_ranking/len(matching)
+    ## END TODO
+
+    assert type(avg_suitor_ranking) == float
+
+    return avg_suitor_ranking
+
+def avg_reviewer_ranking(reviewer_prefs: Dict[str, List[str]], matching: Dict[str, str]) -> float:
+    '''
+        Calculate the average ranking of reviewer in the matching
+        
+        Parameters:
+        
+        reviewer_prefs: dict - Dictionary of reviewer preferences
+        matching: dict - Dictionary of matching
+        
+        Returns:
+        
+        avg_reviewer_ranking: float - Average ranking of reviewer
+    '''
+
+    avg_reviewer_ranking = 0
+
+    ## TODO: Implement the average reviewer ranking calculation
+    for suitor in matching.keys():
+        avg_reviewer_ranking = avg_reviewer_ranking+reviewer_prefs[matching[suitor]].index(suitor) + 1
+
+    avg_reviewer_ranking = avg_reviewer_ranking/len(matching)
+
+    ## END TODO
+
+    assert type(avg_reviewer_ranking) == float
+
+    return avg_reviewer_ranking
+
+def get_preferences(file) -> Tuple[Dict[str, List[str]], Dict[str, List[str]]]:
+    '''
+        Get the preferences from the file
+        
+        Parameters:
+        
+        file: file - File containing the preferences
+        
+        Returns:
+        
+        suitor_prefs: dict - Dictionary of suitor preferences
+        reviewer_prefs: dict - Dictionary of reviewer preferences
+    '''
+    suitor_prefs = {}
+    reviewer_prefs = {}
+
+    for line in file:
+        if line[0].islower():
+            reviewer, prefs = line.strip().split(' : ')
+            reviewer_prefs[reviewer] = prefs.split()
+
+        else:
+            suitor, prefs = line.strip().split(' : ')
+            suitor_prefs[suitor] = prefs.split()
+        
+    return suitor_prefs, reviewer_prefs
+
 
 if __name__ == '__main__':
-    mnist_data = 'mnist.pkl'
-    with open(mnist_data, 'rb') as f:
-        data = pkl.load(f)
-    X=data[0]
-    y=data[1]
-    k=10
-    lda = LDA(k)
-    w=lda.fit(X, y)
-    X_lda = lda.transform(X,w)
+
+    avg_suitor_ranking_list = []
+    avg_reviewer_ranking_list = []
+
+    for i in range(100):
+        with open('data/data_'+str(i)+'.txt', 'r') as f:
+            suitor_prefs, reviewer_prefs = get_preferences(f)
+
+            # suitor_prefs = {
+            #     'A': ['a', 'b', 'c'],
+            #     'B': ['c', 'b', 'a'],
+            #     'C': ['c', 'a', 'b']
+            # }
+
+            # reviewer_prefs = {
+            #     'a': ['A', 'C', 'B'],
+            #     'b': ['B', 'A', 'C'],
+            #     'c': ['B', 'A', 'C']
+            # }
+
+            matching = Gale_Shapley(suitor_prefs, reviewer_prefs)
+
+            avg_suitor_ranking_list.append(avg_suitor_ranking(suitor_prefs, matching))
+            avg_reviewer_ranking_list.append(avg_reviewer_ranking(reviewer_prefs, matching))
+
+    plt.hist(avg_suitor_ranking_list, bins=10, label='Suitor', alpha=0.5, color='r')
+    plt.hist(avg_reviewer_ranking_list, bins=10, label='Reviewer', alpha=0.5, color='g')
+
+    plt.xlabel('Average Ranking')
+    plt.ylabel('Frequency')
+
+    plt.legend()
+    plt.savefig('q2.png')
+
+
+    
+
